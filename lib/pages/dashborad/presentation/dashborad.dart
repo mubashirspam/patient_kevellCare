f77e47b9a5be2fb8a9af47219aa3277f9dc3e240
Madhuri <madhuri.g@kevells.com>
2023-07-26 12:39:24 +0530

    history

--- lib/pages/dashborad/presentation/dashborad.dart
+++ lib/pages/dashborad/presentation/dashborad.dart
@@ -12,6 +12,7 @@ import '../../home_screen/presentation/widgets/home_appbar.dart';
 import '../../profile/presenation/profile_screen.dart';
 import '../../profile/presenation/widgets/profile_appbar.dart';
 
+import '../../report_screen/presentation/widget/history_screen.dart';
 import 'widgets/bottom_navigation.dart';
 
 class Dashboard extends StatefulWidget {
@@ -40,7 +41,7 @@ class _DashboardState extends State<Dashboard> {
       "appbar": const ChatAppBar(),
       "flotingButton": null
     },
-    {"page": const HomeScreen(), "appbar": null, "flotingButton": null},
+    {"page": const History(), "appbar": null, "flotingButton": null},
     {
       "page": const ProfileScreen(),
       "appbar": const ProfileAppBar(),
